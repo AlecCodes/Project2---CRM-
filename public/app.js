c748dc8f0f0f09062cfe5504af0cf4f12446adce
Alec Hannaford <alec322@gmail.com>
2022-12-10 20:05:21 -0800

    added message box

---
+++ public/app.js
@@ -0,0 +1,15 @@
+const notes = document.querySelectorAll('#notification')
+const xbutton = document.querySelectorAll('span')
+
+function remove(){
+    this.parentNode.remove()
+}
+
+// for (let note of notes){
+//     note.style.backgroundColor = 'red'
+// }
+
+for (let button of xbutton){
+    button.style.backgroundColor = 'red'
+    button.addEventListener('click', remove)
+}
