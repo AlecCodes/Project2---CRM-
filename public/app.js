ad15b11ba7774c60397754134be94834fb3ac3c9
Alec Hannaford <alec322@gmail.com>
2022-12-11 10:04:16 -0800

    moved message box stuff to code graveyard

--- public/app.js
+++ public/app.js
@@ -1,24 +1,6 @@
-const notes = document.querySelectorAll('#notification')
-const xbutton = document.querySelectorAll('#X')
 const lastContactElement = document.querySelectorAll("#lastContact")
 const today = new Date()
 
-function remove(){
-    this.parentNode.remove()
-}
-
-// for (let note of notes){
-//     note.style.backgroundColor = 'red'
-// }
-
-for (let button of xbutton){
-    button.style.backgroundColor = 'red'
-    button.addEventListener('click', remove)
-}
-
-if (!notes.length){
-    console.log('empty!')
-}
 
 for (const i of lastContactElement){
    const lastContactDate = new Date(i.innerHTML)
