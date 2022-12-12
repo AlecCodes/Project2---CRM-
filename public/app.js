7a24918f4881df9d364ae7d62fe610ddf693b621
Alec Hannaford <alec322@gmail.com>
2022-12-11 16:35:36 -0800

    red yellow n green color coding for age of correspondance

--- public/app.js
+++ public/app.js
@@ -7,4 +7,11 @@ for (const i of lastContactElement){
    if ((lastContactDate)&& ((today - lastContactDate)/86400000 > 14)){
     i.style.backgroundColor='red'
    }
+   if ((lastContactDate)&& ((today - lastContactDate)/86400000 < 7)){
+    i.style.backgroundColor='green'
+   }
+   if ((lastContactDate)&& ((today - lastContactDate)/86400000 > 7) && ((today - lastContactDate)/86400000 < 14)){
+    i.style.backgroundColor='yellow'
+   }
+
 }
